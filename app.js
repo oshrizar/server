7d28f89f63c12a6a246fa01249cf3aa21def041d
Oshri Zarbiv <oshrizar086@gmail.com>
2023-07-30 15:29:39 +0300

    morgan

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+var cors = require("cors");
 const apiRouter = require("./routes/api");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(cors());
+app.use(
+  logger((tokens, req, res) => {
+    return [
+      new Date().toISOString().replace("T", " "),
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
